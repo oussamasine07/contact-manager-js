351c50c60dbf46a7766da6e135279c03bd1924b4
oussamasine07 <sineoussama@gmail.com>
2025-01-18 10:14:16 +0100

    validate empty fields

--- js/app.js
+++ js/app.js
@@ -14,3 +14,36 @@ closeAddContact.addEventListener("click", () => {
     addContactModal.classList.add("hidden");
     addContactModal.classList.add("z-10");
 });
+
+// select all input elements
+const firstNameInput = document.getElementById("first-name");
+const lastNameInput = document.getElementById("last-name");
+const emailInput = document.getElementById("email");
+const genderFemaleRadio = document.getElementById("gender-female");
+const genderMaleRadio = document.getElementById("gender-male");
+const cityInput = document.getElementById("city");
+const phoneInput = document.getElementById("phone");
+
+// declare error object
+let error = {
+    isError: false,
+    message: ""
+}
+
+// validate empty
+const isEmpty = ( input ) => {
+    if ( input.value == "" ) {
+        error = {
+            isError: true,
+            message: `the ${ input.dataset.inputName } fiels is required`
+        }
+    }
+    return error;
+}
+
+// validate email
+const isValidaEmail = ( input ) => {
+    let inputValue = input.value;
+
+}
+// validate text
